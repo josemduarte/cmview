6f8aaa6429059850354732480faa907d39e7100c
Henning Stehr <h.stehr@tuhh.de>
2007-06-28 15:47:28 +0000

    forgot to add LoadDialogInputError to version control, should be there now

---
+++ cmview/LoadDialogInputError.java
@@ -0,0 +1,22 @@
+package cmview;
+
+public class LoadDialogInputError extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public LoadDialogInputError() {
+	}
+
+	public LoadDialogInputError(String arg0) {
+		super(arg0);
+	}
+
+	public LoadDialogInputError(Throwable arg0) {
+		super(arg0);
+	}
+
+	public LoadDialogInputError(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+}
