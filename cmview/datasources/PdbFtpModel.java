b288cb6b4f3f8ef7a08e64c3177d5d2698f72dd6
Henning Stehr <h.stehr@tuhh.de>
2007-09-17 16:11:17 +0000

    using new constructor from ciffilePdb for loading directly from online pdb

--- cmview/datasources/PdbFtpModel.java
+++ cmview/datasources/PdbFtpModel.java
@@ -1,14 +1,7 @@
 package cmview.datasources;
-import proteinstructure.*;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.zip.*;
+
+import proteinstructure.*;
 
 import cmview.Start;
 
@@ -22,48 +15,10 @@ public class PdbFtpModel extends Model {
 	 * @throws ModelConstructionError 
 	 */
 	public PdbFtpModel(String pdbCode, String pdbChainCode, String edgeType, double distCutoff, int minSeqSep, int maxSeqSep) throws ModelConstructionError {
-		String gzCifFileName = pdbCode+".cif.gz";
-		File gzCifFile = new File(Start.TEMP_DIR,gzCifFileName);
-		gzCifFile.deleteOnExit();
-		File cifFile = new File(Start.TEMP_DIR,pdbCode + ".cif");
-		cifFile.deleteOnExit();
-		String cifFileName = cifFile.getAbsolutePath();
-		
-		// getting gzipped cif file from ftp
+	
+		// load CIF file from online pdb
 		try {
-			URL url = new URL(Start.PDB_FTP_URL+gzCifFileName);
-			URLConnection urlc = url.openConnection();
-			InputStream is = urlc.getInputStream();
-			FileOutputStream os = new FileOutputStream(gzCifFile);
-			int b;
-			while ( (b=is.read())!=-1) {
-				os.write(b);
-			}
-			is.close();
-			os.close();
-		} catch (IOException e) {
-			System.err.println("Error while reading from ftp site "+Start.PDB_FTP_URL);
-			throw new ModelConstructionError(e.getMessage());
-		}
-		
-		// unzipping downloaded file
-		try {
-			GZIPInputStream zis = new GZIPInputStream(new FileInputStream(gzCifFile));
-			FileOutputStream os = new FileOutputStream(cifFileName);
-			int b;
-			while ( (b=zis.read())!=-1) {
-				os.write(b);
-			}
-			zis.close();
-			os.close();
-		} catch (IOException e) {
-			System.err.println("Error while unzipping the cif.gz file.");
-			throw new ModelConstructionError(e.getMessage());
-		}
-		
-		// load CIF file
-		try {
-			this.pdb = new CiffilePdb(cifFileName, pdbChainCode);
+			this.pdb = new CiffilePdb(pdbCode, pdbChainCode, Start.PDB_FTP_URL);
 			this.graph = pdb.get_graph(edgeType, distCutoff);
 
 			super.writeTempPdbFile();
