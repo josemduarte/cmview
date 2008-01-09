c6563a50baf0845163375c4b5016e12f682a4690
Henning Stehr <h.stehr@tuhh.de>
2008-01-09 10:09:50 +0000

    modified info dialog (now showing unique and common contacts and name but not pdb and chain code)

--- cmview/ContactMapInfoDialog.java
+++ cmview/ContactMapInfoDialog.java
@@ -6,7 +6,6 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.TreeMap;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -18,8 +17,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import proteinstructure.Alignment;
-import proteinstructure.IntPairSet;
-import proteinstructure.Pdb;
 
 import cmview.datasources.Model;
 
@@ -77,38 +74,27 @@ public class ContactMapInfoDialog extends JDialog implements ActionListener {
 		this.getRootPane().setDefaultButton(okButton);
 		
 		// data pane
-		int cols = 0;	// will be counted automatically
 		int rows = (mod2==null?2:3);
+		int cols = 0;	// will be counted automatically
+		int showSeqLen = 15;	// how many residues to show in main window
+		
 		JPanel dataPane = new JPanel();
 
 		// initialize data
 		String seq = mod.getSequence();
 		String seq2 = null;
-		String s1 = seq.length() <= 10?(seq.length()==0?"Unknown":seq):seq.substring(0,10) + "...";
+		String s1 = seq.length() <= showSeqLen?(seq.length()==0?"Unknown":seq):seq.substring(0,showSeqLen) + "...";
 		String s2 = "";
-		int numSelectedContacts = 0;
-		if( mod2 == null ) {
-			// settings if mod2 is absent
-			numSelectedContacts = cmPane.getSelContacts().size();
-		} else {
+		int numSelectedContacts = cmPane.getSelContacts().size();
+		if( mod2 != null ) {
 			// settings if mod2 is present
 			seq2 = mod2.getSequence();
-			s2  = seq2.length() <= 10?(seq2.length()==0?"Unknown":seq2):seq2.substring(0,10) + "...";
-
-			TreeMap<ContactMapPane.ContactSelSet, IntPairSet[]> selMap = cmPane.getSelectedContacts(false);
-			IntPairSet union = new IntPairSet();
-			union.addAll(selMap.get(ContactMapPane.ContactSelSet.COMMON)[ContactMapPane.FIRST]);
-			union.addAll(selMap.get(ContactMapPane.ContactSelSet.COMMON)[ContactMapPane.SECOND]);
-			union.addAll(selMap.get(ContactMapPane.ContactSelSet.ONLY_FIRST)[ContactMapPane.FIRST]);
-			union.addAll(selMap.get(ContactMapPane.ContactSelSet.ONLY_FIRST)[ContactMapPane.SECOND]);
-			union.addAll(selMap.get(ContactMapPane.ContactSelSet.ONLY_SECOND)[ContactMapPane.SECOND]);
-			union.addAll(selMap.get(ContactMapPane.ContactSelSet.ONLY_SECOND)[ContactMapPane.FIRST]);
-			
-			numSelectedContacts = union.size();
+			s2  = seq2.length() <= showSeqLen?(seq2.length()==0?"Unknown":seq2):seq2.substring(0,showSeqLen) + "...";
 		}
-		String pdbCode = null, pdbCode2 = null;
-		String chainCode = null, chainCode2 = null;
-		String model = null, model2 = null;
+
+//		String pdbCode = null, pdbCode2 = null;
+//		String chainCode = null, chainCode2 = null;
+//		String model = null, model2 = null;
 		String contactType = null, contactType2 = null;
 		String distCutoff = null, distCutoff2 = null;
 		String minSeqSepStr = null, maxSeqSepStr = null;
@@ -119,10 +105,11 @@ public class ContactMapInfoDialog extends JDialog implements ActionListener {
 		String directed = null, directed2 = null;
 		String secStrucSrc = null, secStrucSrc2 = null;
 		String commonContactsStr = null, selectedContactsStr = null;
+		String uniqueContactsStr1 = null, uniqueContactsStr2 = null;
 		
-		pdbCode = (mod.getPDBCode()==Pdb.NO_PDB_CODE?"none":mod.getPDBCode());
-		chainCode = (mod.getChainCode()==Pdb.NO_CHAIN_CODE?"none":mod.getChainCode());
-		model = Integer.toString(mod.getPdbModelNumber());
+//		pdbCode = (mod.getPDBCode()==Pdb.NO_PDB_CODE?"none":mod.getPDBCode());
+//		chainCode = (mod.getChainCode()==Pdb.NO_CHAIN_CODE?"none":mod.getChainCode());
+//		model = Integer.toString(mod.getPdbModelNumber());
 		contactType = mod.getContactType();
 		distCutoff = Double.toString(mod.getDistanceCutoff());
 		minSeqSepStr = (mod.getMinSequenceSeparation()<1?"none":Integer.toString(mod.getMinSequenceSeparation()));
@@ -134,9 +121,9 @@ public class ContactMapInfoDialog extends JDialog implements ActionListener {
 		secStrucSrc = mod.getSecondaryStructure().getComment();
 		selectedContactsStr = Integer.toString(numSelectedContacts);
 		if(mod2 != null) {
-			pdbCode2 = (mod2.getPDBCode()==Pdb.NO_PDB_CODE?"none":mod2.getPDBCode());
-			chainCode2 = (mod2.getChainCode()==Pdb.NO_CHAIN_CODE?"none":mod2.getChainCode());
-			model2 = Integer.toString(mod2.getPdbModelNumber());
+//			pdbCode2 = (mod2.getPDBCode()==Pdb.NO_PDB_CODE?"none":mod2.getPDBCode());
+//			chainCode2 = (mod2.getChainCode()==Pdb.NO_CHAIN_CODE?"none":mod2.getChainCode());
+//			model2 = Integer.toString(mod2.getPdbModelNumber());
 			contactType2 = mod2.getContactType();
 			distCutoff2 = Double.toString(mod2.getDistanceCutoff());
 			minSeqSepStr2 = (mod2.getMinSequenceSeparation()<1?"none":Integer.toString(mod2.getMinSequenceSeparation()));
@@ -146,32 +133,51 @@ public class ContactMapInfoDialog extends JDialog implements ActionListener {
 			numContacts2 = Integer.toString(mod2.getNumberOfContacts());
 			directed2 = (mod2.isDirected()?"Yes":"No");
 			secStrucSrc2 = mod2.getSecondaryStructure().getComment();
-			commonContactsStr = Integer.toString(cmPane.getCommonContacts(1, 2).size());
+			int commonContacts = cmPane.getCommonContacts(1, 2).size();
+			commonContactsStr = Integer.toString(commonContacts);
+			uniqueContactsStr1 = Integer.toString(mod.getNumberOfContacts() - commonContacts);
+			uniqueContactsStr2 = Integer.toString(mod2.getNumberOfContacts() - commonContacts);
 		}
 		
-		// pdb code
-		dataPane.add(new JLabel("Pdb code:"));
-		dataPane.add(new JLabel(pdbCode));
+		// contact map ids
+		dataPane.add(new JLabel("Name:"));
+		dataPane.add(new JLabel(mod.getLoadedGraphID()));
 		if(mod2 != null) {
-			dataPane.add(new JLabel(pdbCode2));
+			dataPane.add(new JLabel(mod2.getLoadedGraphID()));
 		}
 		rows++;
 		
-		// chain code
-		dataPane.add(new JLabel("Chain code:"));
-		dataPane.add(new JLabel(chainCode));
+		// blank line
+		dataPane.add(new JLabel(" "));
+		dataPane.add(new JLabel(" "));
 		if(mod2 != null) {
-			dataPane.add(new JLabel(chainCode2));
+			dataPane.add(new JLabel(" "));
 		}
 		rows++;	
 		
-		// pdb model number
-		dataPane.add(new JLabel("Model:"));
-		dataPane.add(new JLabel(model));
-		if(mod2 != null) {
-			dataPane.add(new JLabel(model2));
-		}
-		rows++;	
+//		// pdb code
+//		dataPane.add(new JLabel("Pdb code:"));
+//		dataPane.add(new JLabel(pdbCode));
+//		if(mod2 != null) {
+//			dataPane.add(new JLabel(pdbCode2));
+//		}
+//		rows++;
+//		
+//		// chain code
+//		dataPane.add(new JLabel("Chain code:"));
+//		dataPane.add(new JLabel(chainCode));
+//		if(mod2 != null) {
+//			dataPane.add(new JLabel(chainCode2));
+//		}
+//		rows++;	
+//		
+//		// pdb model number
+//		dataPane.add(new JLabel("Model:"));
+//		dataPane.add(new JLabel(model));
+//		if(mod2 != null) {
+//			dataPane.add(new JLabel(model2));
+//		}
+//		rows++;	
 		
 		// contact type
 		dataPane.add(new JLabel("Contact type:"));
@@ -213,14 +219,28 @@ public class ContactMapInfoDialog extends JDialog implements ActionListener {
 		}
 		rows++;	
 		
-		// contact map size
-		dataPane.add(new JLabel("Contact map size:"));
+		// sequence
+		dataPane.add(new JLabel("Sequence:"));
+		JLabel seqLabel1 = new JLabel(s1);
+		String toolTipText = formatSequenceForHtml(seq);
+		seqLabel1.setToolTipText(toolTipText);
+		dataPane.add(seqLabel1);
+		if(mod2 != null) {
+			JLabel seqLabel2 = new JLabel(s2);
+			toolTipText = formatSequenceForHtml(seq2);
+			seqLabel2.setToolTipText(toolTipText);
+			dataPane.add(seqLabel2);
+		}
+		rows++;
+		
+		// sequence length (= contact map size)
+		dataPane.add(new JLabel("Sequence length:"));
 		dataPane.add(new JLabel(contactMapSize));
 		if(mod2 != null) {
 			dataPane.add(new JLabel(contactMapSize2));
 		}
 		rows++;
-		
+				
 		// unobserved residues
 		dataPane.add(new JLabel("Unobserved residues:"));
 		dataPane.add(new JLabel(unobservedResidues));
@@ -228,17 +248,27 @@ public class ContactMapInfoDialog extends JDialog implements ActionListener {
 			dataPane.add(new JLabel(unobservedResidues2));
 		}
 		rows++;	
-		
-		// number of contacts
-		dataPane.add(new JLabel("Number of contacts:"));
-		dataPane.add(new JLabel(numContacts));
+				
+		// secondary structure source
+		dataPane.add(new JLabel("Secondary structure source:"));
+		dataPane.add(new JLabel(secStrucSrc));
 		if(mod2 != null) {
-			dataPane.add(new JLabel(numContacts2));
+			dataPane.add(new JLabel(secStrucSrc2));
 		}
-		rows++;			
-		
-		// is directed
+		rows++;
+
 		if(Start.INCLUDE_GROUP_INTERNALS) {
+
+			// blank line
+			dataPane.add(new JLabel(" "));
+			dataPane.add(new JLabel(" "));
+			if(mod2 != null) {
+				dataPane.add(new JLabel(" "));
+			}
+			rows++;
+
+			// is directed
+
 			dataPane.add(new JLabel("Directed:"));
 			dataPane.add(new JLabel(directed));
 			if(mod2 != null) {
@@ -255,42 +285,32 @@ public class ContactMapInfoDialog extends JDialog implements ActionListener {
 		}
 		rows++;
 		
-		// sequence
-		dataPane.add(new JLabel("Sequence:"));
-		JLabel seqLabel1 = new JLabel(s1);
-		String toolTipText = formatSequenceForHtml(seq);
-		seqLabel1.setToolTipText(toolTipText);
-		dataPane.add(seqLabel1);
+		// number of contacts
+		dataPane.add(new JLabel("Number of contacts:"));
+		dataPane.add(new JLabel(numContacts));
 		if(mod2 != null) {
-			JLabel seqLabel2 = new JLabel(s2);
-			toolTipText = formatSequenceForHtml(seq2);
-			seqLabel2.setToolTipText(toolTipText);
-			dataPane.add(seqLabel2);
+			dataPane.add(new JLabel(numContacts2));
 		}
 		rows++;
 		
-		// secondary structure source
-		dataPane.add(new JLabel("Secondary structure from:"));
-		dataPane.add(new JLabel(secStrucSrc));
 		if(mod2 != null) {
-			dataPane.add(new JLabel(secStrucSrc2));
-		}
-		rows++;
 		
-		// blank line
-		dataPane.add(new JLabel(" "));
-		dataPane.add(new JLabel(" "));
-		if(mod2 != null) {
-			dataPane.add(new JLabel(" "));
-		}
-		rows++;
-		
-		// number of common contacts
-		if(mod2 != null) {
-			dataPane.add(new JLabel("Number of common contacts:"));
-			dataPane.add(new JLabel(commonContactsStr));
-			dataPane.add(new JLabel(" "));		
-			rows++;
+			// number of unique contacts
+			if(mod2 != null) {
+				dataPane.add(new JLabel("Number of unique contacts:"));
+				dataPane.add(new JLabel(uniqueContactsStr1));
+				dataPane.add(new JLabel(uniqueContactsStr2));			
+				rows++;
+			}		
+
+			// number of common contacts
+			if(mod2 != null) {
+				dataPane.add(new JLabel("Number of common contacts:"));
+				dataPane.add(new JLabel(commonContactsStr));
+				dataPane.add(new JLabel(commonContactsStr));		
+				rows++;
+			}
+			
 		}
 		
 		// number of selected contacts
