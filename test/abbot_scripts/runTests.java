24ccf4df5891024a01534d4798523e07a07f1458
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2008-02-01 16:29:46 +0000

    Added java test runer. Changed relative path in test scripts so that files can be run correctly from eclipse with the java tester runTests

---
+++ test/abbot_scripts/runTests.java
@@ -0,0 +1,30 @@
+package test.abbot_scripts;
+
+import java.io.File;
+
+import junit.extensions.abbot.*;
+import junit.framework.Test;
+
+
+public class runTests extends ScriptFixture {
+
+    /** Name is the name of a script filename. */
+    public runTests(String name) {
+        super(name);
+    }
+
+    /** Return the set of scripts we want to run. */
+    public static Test suite() {
+        return new ScriptTestSuite(runTests.class, "test/abbot_scripts") {
+            /** Determine whether the given script should be included. */
+            public boolean accept(File file) {
+                String name = file.getName();
+                return name.startsWith("load_from_casp_rr");
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        TestHelper.runTests(args, runTests.class);
+    }
+}
