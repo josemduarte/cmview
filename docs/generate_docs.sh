7a713b002b6a00755f8226198fb047409760b887
Henning Stehr <h.stehr@tuhh.de>
2008-02-08 11:22:21 +0000

    Fixed bugs in generate_docs.sh

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -4,6 +4,7 @@
 webhome=/home/web/lappe/cmview/
 
 # Generating HTML for web
+echo Generating web pages...
 for basename in tutorial manual installation
 do
 docbook2html.sh docbook/$basename.xml > web/$basename.temp
@@ -12,14 +13,17 @@ python web/create_cmview_web_page.py -t web/template.html -s web/$basename.tidy 
 cp -f web/$basename.html $webhome
 rm -f web/$basename.*
 done
+echo Updating website...
 cp -f images/* $webhome/images/
 
 # Generating JavaHelp
+echo Generating JavaHelp...
 docbook2javahelp.sh docbook/manual.xml -o ../src/resources/help/
-cp ../src/resources/help/index.html ../bin/resources/help/index.html
+cp -f ../src/resources/help/* ../bin/resources/help/
 
 # Generating PDF documentation
+echo Generating PDF documentation...
 for basename in tutorial manual installation
 do
-docbook2pdf.sh docbook/$basename.pdf pdf/$basename.pdf
+docbook2pdf.sh docbook/$basename.xml pdf/$basename.pdf
 done
