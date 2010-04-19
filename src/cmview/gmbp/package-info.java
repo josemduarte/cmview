ec16476d13c960d41ea43770018f279979c7fbc5
Henning Stehr <h.stehr@tuhh.de>
2010-04-19 09:48:39 +0000

    Adding package-info.java file

---
+++ src/cmview/gmbp/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains classes related to a Geometric Multi Body Potential (hence the name GMBP).
+ * In CMView this function is available by right-clicking on a cell in the contact map
+ * and choosing 'Show sphoxel and neighbourhood-traces' from the pop-up menu.
+ * @author Corinna Vehlow
+ */
+package cmview.gmbp;
